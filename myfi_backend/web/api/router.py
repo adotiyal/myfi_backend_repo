4cd2465ebf102bbd3cb52b1fa9c0672e9a9d934b
Arvind Dotiyal <arvind.dotiyal@gmail.com>
2023-11-18 09:41:55 +0530

    Swagger documentation

--- myfi_backend/web/api/router.py
+++ myfi_backend/web/api/router.py
@@ -1,10 +1,11 @@
 from fastapi.routing import APIRouter
 
-from myfi_backend.web.api import docs, dummy, echo, monitoring, redis
+from myfi_backend.web.api import docs, dummy, echo, monitoring, otp, redis
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
+api_router.include_router(otp.router, prefix="/otp", tags=["otp"])
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
 api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
 api_router.include_router(redis.router, prefix="/redis", tags=["redis"])
